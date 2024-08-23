2cd01c95c0c55b3fd99b688e44873ca2275cd9fb
hesventhkumar <hesventh@gmail.com>
2024-08-23 16:35:40 +0530

    initilize

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 
 import Shope from './shop'
 
+
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
      <Shope/>
+     
+     
+     
   </React.StrictMode>
 );
 
--- src/shop.jsx
+++ src/shop.jsx
@@ -2,35 +2,99 @@ import React from 'react'
 import ReactDOM from 'react-dom';
 import {useState,useReducer} from 'react';
 
-import Apple from './image/harvest-7458975_640.jpg'
-
-import Orange from './image/tangerines-1721590_640.jpg'
-import Mango from './image/mango-8283268_640.jpg'
-import Water from './image/watermelons-5318938_640.jpg'
-import Grape from './image/vines-1747224_640.jpg'
-import Banana from './image/bananas-614090_640.jpg'
+import Apple from './image/apple.jpg'
+import Strawberry from './image/strawberry.jpg'
+import Mango from './image/mango.jpg'
+import Watermelon from './image/watermelons.jpg'
+import Pineapple from './image/pineapple.jpg'
+import Banana from './image/bananas.jpg'
 
 
 // INITILIZE STATE
 
-const initialState = { count: 0 };
+const initialState1 = { count: 0 };
+const initialState2 = { count: 0 };
+const initialState3 = { count: 0 };
+const initialState4 = { count: 0 };
+const initialState5 = { count: 0 };
+const initialState6 = { count: 0 };
 
 
 // FUNCTION  REDUCER
 
-function Welcome(state, action) {
+function Welcome1(state1, action) {
   switch (action.category) {
     case 'increment':
-      return { count: state.count + 1 };
+      return { count: state1.count + 1 };
     case 'decrement':
-      return { count: state.count - 1 };
+      return { count: state1.count>0 ? state1.count -1: 0 };
+    default:
+      throw new Error();
+  }
+}
+function Welcome2(state2, action) {
+  switch (action.category) {
+    case 'increment':
+      return { count: state2.count + 1 };
+    case 'decrement':
+      return { count: state2.count>0 ? state2.count -1: 0 };
+    default:
+      throw new Error();
+  }
+}
+function Welcome3(state3, action) {
+  switch (action.category) {
+    case 'increment':
+      return { count: state3.count + 1 };
+    case 'decrement':
+      return { count: state3.count>0 ? state3.count -1: 0 };
+    default:
+      throw new Error();
+  }
+}
+function Welcome4(state4, action) {
+  switch (action.category) {
+    case 'increment':
+      return { count: state4.count + 1 };
+    case 'decrement':
+      return { count: state4.count>0 ? state4.count -1: 0 };
+    default:
+      throw new Error();
+  }
+}
+function Welcome5(state5, action) {
+  switch (action.category) {
+    case 'increment':
+      return { count: state5.count + 1 };
+    case 'decrement':
+      return { count: state5.count>0 ? state5.count -1: 0 };
+    default:
+      throw new Error();
+  }
+}
+function Welcome6(state6, action) {
+  switch (action.category) {
+    case 'increment':
+      return { count: state6.count + 1 };
+    case 'decrement':
+      return { count: state6.count>0 ? state6.count -1: 0 };
     default:
       throw new Error();
   }
 }
 
+
+function Price (){
+
+}
+
 const Shop = () => {
-    const [state, dispatch] = React.useReducer(Welcome, initialState);
+    const [state1, dispatch1] = React.useReducer(Welcome1, initialState1);
+    const [state2, dispatch2] = React.useReducer(Welcome2, initialState2);
+    const [state3, dispatch3] = React.useReducer(Welcome3, initialState3);
+    const [state4, dispatch4] = React.useReducer(Welcome4, initialState4);
+    const [state5, dispatch5] = React.useReducer(Welcome5, initialState5);
+    const [state6, dispatch6] = React.useReducer(Welcome6, initialState6);
 
   return (
     <div>
@@ -51,9 +115,9 @@ const Shop = () => {
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">An item</li>
                     <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+                    <button onClick={() => dispatch1({ category: 'increment' })}>+</button>
+                    <input type="text" value={ state1.count} className='w-25 mx-3 text-center' />
+                    <button onClick={() => dispatch1({ category: 'decrement' })}>-</button>
                     </li>
                     <li class="list-group-item">
                        <b> Rate:</b>₹120/Kg
@@ -70,17 +134,17 @@ const Shop = () => {
                 </div>
                 <div className="col">
                 <div class="card " >
-                <img src={Water} alt=""  class="card-img-top"/>
+                <img src={Strawberry} alt=""  class="card-img-top"/>
                 <div class="card-body">
-                    <h5 class="card-title">Watermelon</h5>
+                    <h5 class="card-title">Strawberry</h5>
                     <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
                  </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">An item</li>
                     <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+                    <button onClick={() => dispatch2({ category: 'increment' })}>+</button>
+                    <input type="text" value={ state2.count} className='w-25 mx-3 text-center' />
+                    <button onClick={() => dispatch2({ category: 'decrement' })}>-</button>
                     </li>
                     <li class="list-group-item">
                        <b> Rate:</b>₹50/Kg
@@ -103,9 +167,9 @@ const Shop = () => {
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">An item</li>
                     <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+                    <button onClick={() => dispatch3({ category: 'increment' })}>+</button>
+                    <input type="text" value={ state3.count} className='w-25 mx-3 text-center' />
+                    <button onClick={() => dispatch3({ category: 'decrement' })}>-</button>
                     </li>
                     <li class="list-group-item">
                        <b> Rate:</b>₹90/Kg
@@ -127,17 +191,17 @@ const Shop = () => {
            <div className="row m-3">
                 <div className="col">
                 <div class="card " >
-                <img src={Apple} alt=""  class="card-img-top"/>
+                <img src={Watermelon} alt=""  class="card-img-top"/>
                 <div class="card-body">
-                    <h5 class="card-title">Apple</h5>
+                    <h5 class="card-title">Watermelon</h5>
                     <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
                  </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">An item</li>
                     <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+                    <button onClick={() => dispatch4({ category: 'increment' })}>+</button>
+                    <input type="text" value={ state4.count} className='w-25 mx-3 text-center' />
+                    <button onClick={() => dispatch4({ category: 'decrement' })}>-</button>
                     </li>
                     <li class="list-group-item">
                        <b> Rate:</b>₹120/Kg
@@ -154,17 +218,17 @@ const Shop = () => {
                 </div>
                 <div className="col">
                 <div class="card " >
-                <img src={Banana} alt=""  class="card-img-top"/>
+                <img src={Pineapple} alt=""  class="card-img-top"/>
                 <div class="card-body">
-                    <h5 class="card-title">Banana</h5>
+                    <h5 class="card-title">Pineapple</h5>
                     <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
                  </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">An item</li>
                     <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+                    <button onClick={() => dispatch5({ category: 'increment' })}>+</button>
+                    <input type="text" value={ state5.count} className='w-25 mx-3 text-center' />
+                    <button onClick={() => dispatch5({ category: 'decrement' })}>-</button>
                     </li>
                     <li class="list-group-item">
                        <b> Rate:</b>₹50/Kg
@@ -179,17 +243,17 @@ const Shop = () => {
                 </div>
                 <div className="col">
                 <div class="card " >
-                <img src={Mango} alt=""  class="card-img-top"/>
+                <img src={Banana} alt=""  class="card-img-top"/>
                 <div class="card-body">
-                    <h5 class="card-title">Mango</h5>
+                    <h5 class="card-title">Banana</h5>
                     <p class="card-text">Lorem, ipsum dolor sit amet consectetur adipisicing elit.</p>
                  </div>
                 <ul class="list-group list-group-flush">
                     <li class="list-group-item">An item</li>
                     <li class="list-group-item">
-                    <button onClick={() => dispatch({ category: 'increment' })}>+</button>
-                    <input type="text" value={ state.count} className='w-25 mx-3 text-center' />
-                    <button onClick={() => dispatch({ category: 'decrement' })}>-</button>
+                    <button onClick={() => dispatch6({ category: 'increment' })}>+</button>
+                    <input type="text" value={ state6.count} className='w-25 mx-3 text-center' />
+                    <button onClick={() => dispatch6({ category: 'decrement' })}>-</button>
                     </li>
                     <li class="list-group-item">
                        <b> Rate:</b>₹90/Kg
@@ -207,7 +271,83 @@ const Shop = () => {
 
 
 
+
+
+
+                                          {/* BILL DETAIL */}
+
+<div className='h2 text-center'> Bill-Detail</div>
+
+
+        <div className="container text-center border border-2 m-3 p-3">
+          <div className="row mb-2">
+            <div className="col fw-bold">S.no</div>
+
+            <div className="col fw-bold">Name</div>
+            <div className="col fw-bold">Qty</div>
+            <div className="col fw-bold">Total</div>
+          </div>
+
+          <div className="row">
+            <div className="col">1</div>
+            <div className="col">Apple</div>
+            <div className="col">{ state1.count}</div>
+            <div className="col">₹{state1.count*120}</div>
+          </div>
+
+          <div className="row">
+          <div className="col">2</div>
+            <div className="col">Strawberry</div>
+            {/* <div className="col">Qty</div> */}
+            <div className="col">{ state2.count}</div>
+            <div className="col">₹{state2.count*50}</div>
+          </div>
+
+          <div className="row">
+          <div className="col">3</div>
+            <div className="col">Mango</div>
+            {/* <div className="col">Qty</div> */}
+            <div className="col">{ state3.count}</div>
+            <div className="col">₹{state3.count*90}</div>
+          </div>
+
+          <div className="row">
+          <div className="col">4</div>
+            <div className="col">Watermelon</div>
+            {/* <div className="col">Qty</div> */}
+            <div className="col">{ state4.count}</div>
+            <div className="col">₹{state4.count*120}</div>
+          </div>
+          <div className="row">
+          <div className="col">5</div>
+            <div className="col">Pineapple</div>
+            {/* <div className="col">Qty</div> */}
+            <div className="col">{ state5.count}</div>
+            <div className="col">₹{state5.count*50}</div>
+          </div>
+          <div className="row">
+          <div className="col">6</div>
+            <div className="col">Banana</div>
+            {/* <div className="col">Qty</div> */}
+            <div className="col">{ state6.count}</div>
+            <div className="col">₹{state6.count*90}</div>
+          </div>
+          <hr />
+          <div className="row ">
+          <div className="col-10  fw-bold text-start fs-4">TOTAL:</div>
+            <div className="col-2 fw-bold text-danger  text-start fs-4">{state1.count*120+state2.count*50+state3.count*90+state4.count*120+state5.count*50+state6.count*90}</div>
+            
         </div>
+
+        </div>
+
+       
+
+
+
+        </div>
+
+         
 
 
 
